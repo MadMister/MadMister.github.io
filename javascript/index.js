3a66b05fefb09cad1dcbe7b6eee92b86210fc34c
MadMister <31894131+MadMister@users.noreply.github.com>
2020-12-01 19:20:56 +0100

    s

---
+++ javascript/index.js
@@ -0,0 +1,35 @@
+var x = document.getElementsByClass("animated");
+
+function expand() {
+    x.style.display = block;
+    let h = 0;
+    let i = setInterval(frame, 500);
+    function frame() {
+        if (h === 200) {
+            clearInterval(i);
+        }
+        else {
+            h++;
+            x.style.height = h + 'px';
+        }
+    }
+}
+
+function contract() {
+    let h = x.style.height;
+    let i = setInterval(frame, 500);
+    function frame() {
+        if (h === 0) {
+            x.style.display = none;
+            clearInterval(i);
+        }
+        else {
+            h--;
+            x.style.height = h + 'px';
+        }
+    }
+}
+
+x.addEventListener("mouseover", expand);
+
+x.addEventListener("mouseout", contract);
