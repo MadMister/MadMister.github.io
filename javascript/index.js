438f99a57f59f91d2928126944f8d0e0a965f11a
MadMister <31894131+MadMister@users.noreply.github.com>
2020-12-13 16:02:36 +0100

    m

---
+++ javascript/index.js
@@ -0,0 +1,6 @@
+var x = document.getElementsByClassName("patch1");
+x[0].addEventListener("click",myFun);
+function myFun() {
+    let y =document.getElementsByClassName("patch2");
+    y[0].style.backgroundColor = "#0f0f0f";
+}
