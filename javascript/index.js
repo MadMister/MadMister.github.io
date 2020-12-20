22e45eb3a2042ab841a3a1950fde44a441289367
MadMister <31894131+MadMister@users.noreply.github.com>
2020-12-20 13:09:00 +0100

    m

--- javascript/index.js
+++ javascript/index.js
@@ -1,51 +1,26 @@
 let tiles = document.querySelectorAll("i.initial > div");
 
 function toggle(a,b) {
-    let r = a - 1;
-    let c = b - 1;
-    let s = a + 1;
-    let d = b + 1;
     
-    let r1 = "row"+r;
-    let c1 = "column"+c;
+    let coor = [[a,b]];
     
-    let r2 = "row"+r;
-    let c2 = "column"+s;
+    coor.push([a,b-1]);
+    coor.push([a,b+1]);
+    coor.push([a-1,b]);
+    coor.push([a+1,b]);
     
-    let tile1 = document.getElementsByClassName("r1 c1");
-    if (tile1.backgroundColor == "black") {
-        tile.backgroundColor = "white";
+    for (let j = 0; j<5 j++) {
+        let r = "row" + coor[j][0];
+        let c = "column" + coor[j][1];
+        let el = document.getElementsByClassName(r c)[0];
+        if (el.backgroundColor == "white") {
+            el.backgroundColor = "black";
+        }
+        else {
+            el.backgroundColor = "black";
+        }
     }
-    else {
-        tile1.backgroundColor = "black";
-    }
-    
-    let tile2 = document.getElementsByClassName("r1 c2");
-    if (tile2.backgroundColor == "black") {
-        tile2.backgroundColor = "white";
-    }
-    else {
-        tile2.backgroundColor = "black";
-    }
-    
-    let tile3 = document.getElementsByClassName("r2 c1");
-    if (tile3.backgroundColor == "black") {
-        tile3.backgroundColor = "white";
-    }
-    else {
-        tile3.backgroundColor = "black";
-    }
-    
-    let tile4 = document.getElementsByClassName("r2 c2");
-    if (tile4.backgroundColor == "black") {
-        tile4.backgroundColor = "white";
-    }
-    else {
-        tile4.backgroundColor = "black";
-    }
-    
 }
-
 for (let i = 0; i < tiles.length; i++) {
     let m = Math.floor(i / 10);
     let n = i % 10;
