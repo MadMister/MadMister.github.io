46b41f5e4175a9a07db4cbee3400740dfd2fa50b
MadMister <31894131+MadMister@users.noreply.github.com>
2020-12-21 13:47:07 +0100

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -21,28 +21,10 @@ function toggle(a,b) {
         }
     }
 }
+
 for (let i = 0; i < tiles.length; i++) {
     let m = Math.floor(i / 10);
     let n = i % 10;
-    tiles[i].addEventListener("click", function(m+1,n+1) {
-        let coor = [[a,b]];
-        
-        coor.push([a,b-1]);
-        coor.push([a,b+1]);
-        coor.push([a-1,b]);
-        coor.push([a+1,b]);
-        
-        for (let j = 0; j<5; j++) {
-            let r = "row" + coor[j][0];
-            let c = "column" + coor[j][1];
-            let el = document.getElementsByClassName(r + " " + c)[0];
-            if (getComputedStyle(el).backgroundColor == "rgb(255, 255, 255)") {
-                el.style.backgroundColor = "rgb(0, 0, 0)";
-            }
-            else {
-                el.style.backgroundColor = "rgb(255, 255, 255)";
-            }
-        }
-                                                 
-    });
+    tiles[i].addEventListener("click", function() {
+        toggle(m+1 ,n+1);
 }
