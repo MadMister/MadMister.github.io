104847c8550825d9310356beac86351648cc27c4
MadMister <31894131+MadMister@users.noreply.github.com>
2020-12-25 10:41:29 +0100

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -1,7 +1,7 @@
 let slices = document.getElementsByClassName("burger-slice");
-let menu = document.getElementsByClassName("burger-menu");
-menu[0].addEventListener("mouseover", function() {
+let menu = document.getElementsByClassName("burger-menu")[0];
+menu.addEventListener("mouseover", function() {
     slices[0].style.animatonPlayState = "running";
     slices[1].style.animatonPlayState = "running";
     slices[2].style.animatonPlayState = "running";
-})
+});
