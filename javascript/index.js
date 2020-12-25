ee8498f50fd7ba4ee16fc09fb7be112e4bd7b20c
MadMister <31894131+MadMister@users.noreply.github.com>
2020-12-25 10:58:57 +0100

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -2,7 +2,7 @@ let slices = document.getElementsByClassName("burger-slice");
 let menu = document.getElementsByClassName("burger-menu")[0];
 menu.addEventListener("mouseover", function() {
     slices[0].style.animationDirection = "normal";
-    slices[1].style.animationPlayState = "normal";
+    slices[1].style.animationDirection = "normal";
                       
     slices[0].style.animationPlayState = "running";
     slices[1].style.animationPlayState = "running";
