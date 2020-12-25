fd012e35963b9ea63feff06ed28412026f22eb4c
MadMister <31894131+MadMister@users.noreply.github.com>
2020-12-25 10:37:19 +0100

    m

--- javascript/index.js
+++ javascript/index.js
@@ -1,42 +1,7 @@
-let tiles = document.querySelectorAll(".initial > div");
-
-function toggle(a,b) {
-    
-    let coor = [[a,b]];
-    
-    if (b !== 1) {
-        coor.push([a,b-1]);
-    }
-    
-    if (b !== 10) {
-        coor.push([a,b+1]);
-    }
-    
-    if (a !== 1) {
-        coor.push([a-1,b]);
-    }
-    
-    if (a !== 10) {
-        coor.push([a+1,b]);
-    }
-    
-    for (let j = 0; j<coor.length; j++) {
-        let r = "row" + coor[j][0];
-        let c = "column" + coor[j][1];
-        let el = document.getElementsByClassName(r + " " + c)[0];
-        if (getComputedStyle(el).backgroundColor == "rgb(255, 255, 255)") {
-            el.style.backgroundColor = "rgb(0, 0, 0)";
-        }
-        else {
-            el.style.backgroundColor = "rgb(255, 255, 255)";
-        }
-    }
-}
-
-for (let i = 0; i < tiles.length; i++) {
-    let m = Math.floor(i / 10);
-    let n = i % 10;
-    tiles[i].addEventListener("click", function() {
-        toggle(m+1 ,n+1);
-    });
-}
+let slices = document.getElementsByClassName("burger-silce");
+let menu = document.getElementsByClassName("burger-menu");
+menu[0].addEventListener("mouseover", function() {
+    slices[0].style.animatonPlayState = "running";
+    slices[1].style.animatonPlayState = "running";
+    slices[2].style.animatonPlayState = "running";
+})
