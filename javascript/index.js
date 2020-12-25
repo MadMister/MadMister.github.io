f8f07c1ab1f344d59aba9d00d4c59dc8c748bd49
MadMister <31894131+MadMister@users.noreply.github.com>
2020-12-25 11:19:16 +0100

    m

--- javascript/index.js
+++ javascript/index.js
@@ -1,19 +1,13 @@
 let slices = document.getElementsByClassName("burger-slice");
 let menu = document.getElementsByClassName("burger-menu")[0];
 menu.addEventListener("mouseover", function() {
-    slices[0].style.animationDirection = "normal";
-    slices[1].style.animationDirection = "normal";
-                      
-    slices[0].style.animationFillMode = "forwards";
-    slices[1].style.animationFillMode = "forwards";
-                      
+    slices[0].style.animationName = "expand";
+    slices[1].style.animationName = "expand";
+    
     slices[0].style.animationPlayState = "running";
     slices[1].style.animationPlayState = "running";
 });
 menu.addEventListener("mouseout", function() {
-    slices[0].style.animationDirection = "reverse";
-    slices[1].style.animationDirection = "reverse";
-                      
-    slices[0].style.animationFillMode = "none";
-    slices[1].style.animationFillMode = "none";
+    slices[0].style.animationName = "contract";
+    slices[1].style.animationName = "contract";
 });
