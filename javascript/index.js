511d3c32dd34b4bcc9f7da4d402611042ac1b5d8
MadMister <31894131+MadMister@users.noreply.github.com>
2020-12-25 11:25:39 +0100

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -1,13 +1,22 @@
 let slices = document.getElementsByClassName("burger-slice");
 let menu = document.getElementsByClassName("burger-menu")[0];
+let contracted = true;
 menu.addEventListener("mouseover", function() {
-    slices[0].style.animationName = "expand";
-    slices[1].style.animationName = "expand";
+    if(expanded) {
+        slices[0].style.animationName = "expand";
+        slices[1].style.animationName = "expand";
+        
+        slices[0].style.animationPlayState = "running";
+        slices[1].style.animationPlayState = "running";
+        
+        contracted = false;
+    }
     
-    slices[0].style.animationPlayState = "running";
-    slices[1].style.animationPlayState = "running";
+    else {
+        slices[0].style.animationName = "contract";
+        slices[1].style.animationName = "contract";
+                      
+        contracted = true;
+    }
 });
-menu.addEventListener("mouseout", function() {
-    slices[0].style.animationName = "contract";
-    slices[1].style.animationName = "contract";
-});
+
