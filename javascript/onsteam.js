5ddfc9643eeea3e7b6129a28b944d7dece593495
MadMister <31894131+MadMister@users.noreply.github.com>
2020-12-26 15:05:03 +0100

    Update onsteam.js

--- javascript/onsteam.js
+++ javascript/onsteam.js
@@ -57,6 +57,9 @@ affiliatesar[0].addEventListener("click", toggleInfo);
 function toggleInfo(e) {
     e.stopPropagation();
     if (j >= 0 && e.target != infoar[j]) {
+        let k =10;
+    }
+    if (j >= 0) {
         infoar[j].style.animationName = "disappear";
     }
     
