78c8282494e5841ab97410a4372000b51dbc8016
MadMister <31894131+MadMister@users.noreply.github.com>
2021-03-13 12:19:28 +0100

    n

--- javascript/index.js
+++ javascript/index.js
@@ -1,45 +1,7 @@
-let slices = document.getElementsByClassName("burger-slice");
-let menu = document.getElementsByClassName("burger-menu")[0];
-let pane = document.getElementsByClassName("navigation-strip")[0];
-let contracted = true;
+let cards = document.getElementsByClassName("card");
 
-
-
-menu.addEventListener("click", function() {
-    if(contracted) {
-        slices[0].style.animationName = "expand";
-        slices[1].style.animationName = "expand";
-        
-        pane.style.animationName = "dropdown";
-        
-        contracted = false;
-    }
-    
-    else {
-        slices[0].style.animationName = "contract";
-        slices[1].style.animationName = "contract";
-                      
-        pane.style.animationName = "rollup";
-                      
-        contracted = true;
-    }
-});
-
-
-let widthMatch = window.matchMedia("(min-width: 700px)");
-widthMatch.addEventListener('change', function(mm) {
-    if (mm.matches) {
-        
-        slices[0].style.animationName = "";
-        slices[1].style.animationName = "";
-        
-        slices[0].style.marginBottom = "10px";
-        slices[1].style.marginBottom = "10px";
-        
-        pane.style.animationName = "";
-        
-        pane.style.height = "0";
-                      
-        contracted = true;
-    }
-});
+for (let i = 0; i < cards.length; i++) {
+    cards[i].addEventListener("click", function() {
+        cards[i].classList.toggle("flipped");
+    });
+}
