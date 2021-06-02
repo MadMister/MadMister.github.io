d82b9f37e4cb0e6daafedcb5785e38224b1b31f1
MadMister <31894131+MadMister@users.noreply.github.com>
2021-06-02 14:10:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
 let cards = document.getElementsByClassName("card");
+let buttons = document.getElementsByClassName("gridButton");
 let j = -1;
+
+function turn(card) {
+    if (j == -1) {
+        j = card;
+    }
+    else if (card == j) {
+        j = -1;
+    }
+    else {
+        cards[j].classList.toggle("flipped");
+        j = card;
+    }
+    cards[card].classList.toggle("flipped");
+}
 for (let i = 0; i < cards.length; i++) {
     
     cards[i].addEventListener("click", function() {
-        
-        if (j == -1) {
-            j = i;
-        }
-        else if (i == j) {
-            j = -1;
-        }
-        else {
-            cards[j].classList.toggle("flipped");
-            j = i;
-        }
-        cards[i].classList.toggle("flipped");
+        turn(i);
+    });
+    buttons[i].addEventListener("click", function() {
+        turn(i);
     });
 }
